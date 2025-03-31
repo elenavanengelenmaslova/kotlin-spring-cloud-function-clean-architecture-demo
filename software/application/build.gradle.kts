b066e89a7ed1a70c1287d131099490c14177d267
elena.vanengelen <elena.vanengelen@edsn.nl>
2025-03-31 16:18:12 +0200

    remove not needed dependency

--- software/application/build.gradle.kts
+++ software/application/build.gradle.kts
@@ -19,7 +19,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.12.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.12.0")
-    testImplementation("org.testcontainers:junit-jupiter:1.20.5")
 }
 
 configurations {
