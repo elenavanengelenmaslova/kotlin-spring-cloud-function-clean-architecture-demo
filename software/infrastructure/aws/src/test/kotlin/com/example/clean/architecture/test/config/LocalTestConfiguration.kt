d0a311bc1cd8a911cfa5731b376d5dfc01b159c1
elena.vanengelen <elena.vanengelen@edsn.nl>
2025-05-27 13:33:19 +0200

    fix tests

--- software/infrastructure/aws/src/test/kotlin/com/example/clean/architecture/test/config/LocalTestConfiguration.kt
+++ software/infrastructure/aws/src/test/kotlin/com/example/clean/architecture/test/config/LocalTestConfiguration.kt
@@ -1,6 +1,7 @@
 package com.example.clean.architecture.test.config
 
 import aws.sdk.kotlin.services.s3.S3Client
+import aws.sdk.kotlin.services.ses.SesClient
 import io.mockk.mockk
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
@@ -10,4 +11,7 @@ class LocalTestConfiguration {
 
     @Bean
     fun s3Client(): S3Client = mockk(relaxed = true)
+
+    @Bean
+    fun sesClient(): SesClient = mockk(relaxed = true)
 }
