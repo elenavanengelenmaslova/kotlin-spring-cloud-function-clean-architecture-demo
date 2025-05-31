d589461a3e3dd189c57a7bef832ea1adaf5a859f
elena.vanengelen <elena.vanengelen@edsn.nl>
2025-05-31 12:06:30 +0200

    remove email, regen terraform

--- software/infrastructure/aws/src/test/kotlin/com/example/clean/architecture/test/config/LocalTestConfiguration.kt
+++ software/infrastructure/aws/src/test/kotlin/com/example/clean/architecture/test/config/LocalTestConfiguration.kt
@@ -1,7 +1,6 @@
 package com.example.clean.architecture.test.config
 
 import aws.sdk.kotlin.services.s3.S3Client
-import aws.sdk.kotlin.services.ses.SesClient
 import io.mockk.mockk
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
@@ -12,6 +11,4 @@ class LocalTestConfiguration {
     @Bean
     fun s3Client(): S3Client = mockk(relaxed = true)
 
-    @Bean
-    fun sesClient(): SesClient = mockk(relaxed = true)
 }
